fd490cb789337344d0f3f0fb8bcd58e09874004f
vikneshwaran <vikneshwaran360@gmail.com>
2024-11-20 13:40:52 +0530

    notification-added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 // src/App.js
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import Loaders from "./components/Loaders/Loaders";
 import routes from "./providers/routes";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { ToastContainer, toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:8080");
 function App() {
+  // const utcDate = new Date(utcDateString);
+  useEffect(() => {
+    socket.on("todoNotification", (data) => {
+      console.log("Notification received:", data);
+      if (data.todos) {
+        const todoTime = new Date(data.todos[0].time);
+        const now = new Date();
+        const timeDifference = todoTime - now;
+        const minutesToDue = Math.floor(timeDifference / 1000 / 60);
+        console.log(data.todos[0].time, "p");
+        if (minutesToDue <= 10 && minutesToDue > 0) {
+          toast(
+            `Your task "${data.todos[0].title}" is due in ${minutesToDue} minutes!`
+          );
+        }
+      }
+    });
+
+    return () => {
+      socket.off("todoNotification");
+    };
+  }, []);
   return (
     <Provider store={store}>
       <ToastContainer
